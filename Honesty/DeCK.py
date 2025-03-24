b03f910e2068f3d4e20ab9c327b014dd8e76e5d1
byronBBL <bibaolong23z@ict.ac.cn>
2025-03-24 08:50:26 +0000

    Initial commit

---
+++ Honesty/DeCK.py
@@ -0,0 +1,137 @@
+from vllm import LLM, SamplingParams
+from transformers import AutoModelForCausalLM, AutoTokenizer, StoppingCriteria
+import torch
+from transformers.generation.stopping_criteria import StoppingCriteriaList
+
+class LLamaQaStoppingCriteria(StoppingCriteria):
+    def __init__(self, list_token_ids_sequence: list = []):
+        self.token_ids_sequences = []
+        self.lengths = []
+        for token_ids_sequence in list_token_ids_sequence:
+            self.token_ids_sequences.append(torch.tensor(token_ids_sequence, dtype=torch.long))
+            self.lengths.append(len(token_ids_sequence))
+        
+    # @add_start_docstrings(STOPPING_CRITERIA_INPUTS_DOCSTRING)
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+        # check the final {self.length} tokens
+        stop = False
+        for token_ids_sequence, length in zip(self.token_ids_sequences, self.lengths):
+            if input_ids.shape[-1] < length:
+                continue
+            else:
+                if bool(torch.all(input_ids[0, -length:] == token_ids_sequence.to(input_ids.device))):
+                    stop = True
+                    break
+        return stop
+    
+class DECK:
+
+    def __init__(self, model_name, device, num_gpus, max_gpu_memory=27):
+        self.model_name = model_name
+        self.device = device
+        self.num_gpus = num_gpus
+        self.stopping_criteria = None
+        self.max_gpu_memory = max_gpu_memory
+
+        self.model, self.tokenizer = self.load_model(model_name)
+
+    def load_model(self, model_name):
+        if self.device == "cuda":
+            kwargs = {"torch_dtype": torch.float16, "offload_folder": f"{model_name}/offload"}
+            if self.num_gpus == "auto":
+                kwargs["device_map"] = "auto"
+            else:
+                self.num_gpus = int(self.num_gpus)
+                if self.num_gpus != 1:
+                    kwargs.update({
+                        "device_map": "auto",
+                        "max_memory": {i: f"{self.max_gpu_memory}GiB" for i in range(self.num_gpus)},
+                    })
+        elif self.device == "cpu":
+            kwargs = {}
+        else:
+            raise ValueError(f"Invalid device: {self.device}")
+        
+        tokenizer = AutoTokenizer.from_pretrained(model_name if not 'vicuna' in model_name else 'huggyllama/llama-7b')
+        model = AutoModelForCausalLM.from_pretrained(model_name,
+            low_cpu_mem_usage=True, **kwargs)
+
+        if self.device == "cuda" and self.num_gpus == 1:
+            model.cuda()
+        
+        return model, tokenizer
+    
+    def set_stop_words(self, stop_words):
+        self.stop_words = stop_words
+        self.stopping_criteria = StoppingCriteriaList()
+        list_stop_word_ids = []
+        for stop_word in self.stop_words:
+            stop_word_ids = self.tokenizer.encode('\n' + stop_word)[3:]
+            list_stop_word_ids.append(stop_word_ids)
+            print("Added stop word: ", stop_word, 'with the ids', stop_word_ids, flush=True)
+        self.stopping_criteria.append(LLamaQaStoppingCriteria(list_stop_word_ids))
+
+    def generate(self, base_prompt, context_prompt, alpha=0.0, max_new_tokens=32, top_p=1, top_k=1, temperature=1.0, mode='base_no_rag', verbose=False, remove_stop_words=False, relative_top=0.1, **kwargs):
+        with torch.no_grad():
+            
+            if mode == 'base_no_rag':
+                assert base_prompt is not None, "base_prompt must be specified"
+                base_ids = self.tokenizer(base_prompt, return_tensors="pt").input_ids.to(self.device)
+                max_len = base_ids.shape[-1] + max_new_tokens
+                outputs = self.model.generate(base_ids, max_length=max_len, num_return_sequences=1,
+                                    output_scores=True, return_dict_in_generate=True, deck_decoding=False,
+                                    top_p=top_p, top_k=top_k, temperature=temperature, stopping_criteria=self.stopping_criteria, **kwargs)
+
+            elif mode == 'baseline':
+                assert context_prompt is not None, "base_prompt must be specified"
+                context_ids = self.tokenizer(context_prompt, return_tensors="pt").input_ids.to(self.device)
+                max_len = context_ids.shape[-1] + max_new_tokens
+                outputs = self.model.generate(context_ids, max_length=max_len, num_return_sequences=1,
+                                    output_scores=True, return_dict_in_generate=True, deck_decoding=False,
+                                    top_p=top_p, top_k=top_k, temperature=temperature, stopping_criteria=self.stopping_criteria, **kwargs)
+            
+            elif mode == 'deck':
+                assert base_prompt is not None, "base_prompt must be specified"
+                assert context_prompt is not None, "context_prompt must be specified"
+                base_ids = self.tokenizer(base_prompt, return_tensors="pt").input_ids.to(self.device)
+                max_len_base = base_ids.shape[-1] + max_new_tokens
+                context_ids = self.tokenizer(context_prompt, return_tensors="pt").input_ids.to(self.device)
+                max_len_context = context_ids.shape[-1] + max_new_tokens
+                max_len = max(max_len_base, max_len_context)
+                outputs = self.model.generate(context_ids, base_ids, alpha = alpha, max_length=max_len, num_return_sequences=1,
+                                        output_scores=True, return_dict_in_generate=True, deck_decoding=True,
+                                        top_p=top_p, top_k=top_k, temperature=temperature, stopping_criteria=self.stopping_criteria, **kwargs,)
+
+
+            sequences, scores = outputs.sequences, outputs.scores
+            # IG_list, entropy_student_lst, entropy_lst = outputs.IG_lst, outputs.entropy_student_lst, outputs.entropy_lst
+            if mode == 'base_no_rag':
+                gen_sequences = sequences[:, base_ids.shape[-1]:][0, :]
+            else: gen_sequences = sequences[:, context_ids.shape[-1]:][0, :]
+            gen_arr = gen_sequences.cpu().numpy()
+
+            output_str = self.tokenizer.decode(gen_sequences, skip_special_tokens=True)
+
+            decoded_tokens = [self.tokenizer.decode([token_id], skip_special_tokens=False) for token_id in gen_sequences]
+
+            check_lst = []
+
+            for _id in range(len(decoded_tokens)):
+                check_lst.append((int(gen_sequences[_id]), decoded_tokens[_id], ))
+
+            if verbose:
+                print('MODEL OUTPUT: \n{0}'.format(output_str))
+
+            if remove_stop_words:
+                for stop_word in self.stop_words:
+                    length_to_remove = len(stop_word)
+                    if output_str[-length_to_remove:] == stop_word:
+                        output_str = output_str[:-length_to_remove]
+                output_str = output_str.strip()
+
+        if self.device:
+            torch.cuda.empty_cache()
+
+        # return output_str
+
+        return output_str
---
+++ Honesty/eval_faith.py
@@ -0,0 +1,233 @@
+import re
+import ast
+import string
+import json
+import re
+import argparse
+from tqdm import tqdm
+import os
+import torch
+from transformers import AutoTokenizer, AutoModel, StoppingCriteria, StoppingCriteriaList, AutoModelForCausalLM
+import logging
+os.environ["MKL_THREADING_LAYER"] = "GNU"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+from DeCK import DECK
+
+class LLamaQaStoppingCriteria(StoppingCriteria):
+    def __init__(self, list_token_ids_sequence: list = []):
+        self.token_ids_sequences = []
+        self.lengths = []
+        for token_ids_sequence in list_token_ids_sequence:
+            self.token_ids_sequences.append(torch.tensor(token_ids_sequence, dtype=torch.long))
+            self.lengths.append(len(token_ids_sequence))
+        
+    # @add_start_docstrings(STOPPING_CRITERIA_INPUTS_DOCSTRING)
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+        # check the final {self.length} tokens
+        stop = False
+        for token_ids_sequence, length in zip(self.token_ids_sequences, self.lengths):
+            if input_ids.shape[-1] < length:
+                continue
+            else:
+                if bool(torch.all(input_ids[0, -length:] == token_ids_sequence.to(input_ids.device))):
+                    stop = True
+                    break
+        return stop
+
+def set_stop_words(tokenizer, stop):
+    stop_words = stop
+    list_stop_word_ids = []
+    for stop_word in stop_words:
+            stop_word_ids = tokenizer.encode('\n' + stop_word)[3:]
+            list_stop_word_ids.append(stop_word_ids)
+            print("Added stop word: ", stop_word, 'with the ids', stop_word_ids, flush=True)
+    stopping_criteria = StoppingCriteriaList()
+    stopping_criteria.append(LLamaQaStoppingCriteria(list_stop_word_ids))
+    return stopping_criteria
+            
+def call_llama(model, tokenizer, prompt, stopping_criteria, stop):
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+    sequences = model.generate(input_ids.cuda(), stopping_criteria = stopping_criteria, max_new_tokens = 512)[0, input_ids.shape[-1]:]
+    decoded = tokenizer.decode(sequences, skip_special_tokens=True)
+    for stop_word in stop:
+        length_to_remove = len(stop_word)
+        if decoded[-length_to_remove:] == stop_word:
+            decoded = decoded[:-length_to_remove]
+    output_str = decoded.strip()
+    return output_str
+
+def call_deck(model, base_prompts, context_prompts, stop, params_dict):
+    
+    sequences = model.generate(base_prompts, context_prompts, **params_dict)
+
+    for stop_word in stop:
+        length_to_remove = len(stop_word)
+        if sequences[-length_to_remove:] == stop_word:
+            sequences = sequences[:-length_to_remove]
+    output_str = sequences.strip()
+    return output_str
+
+def normalize_answer(s):
+    """Lower text and remove punctuation, articles and extra whitespace."""
+    def remove_articles(text):
+        return re.sub(r'\b(a|an|the)\b', ' ', text)
+    def white_space_fix(text):
+        return ' '.join(text.split())
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+    def lower(text):
+        return text.lower()
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+def exact_match_score(prediction, ground_truth):
+    return (normalize_answer(prediction) == normalize_answer(ground_truth))    
+
+def recall_score(prediction, ground_truth):
+    prediction = normalize_answer(prediction)
+    ground_truth = normalize_answer(ground_truth)
+    return (ground_truth in prediction)
+
+def get_score(preds, golds):
+    em, recall = 0, 0
+    for pred, gold in zip(preds, golds):
+        if isinstance(gold, list):
+            _em, _recall = 0, 0
+            for g in gold:
+                _em = max(exact_match_score(pred, g), _em)
+                _recall = max(recall_score(pred, g), _recall)
+            em += _em
+            recall += _recall
+        else:
+            em += exact_match_score(pred, gold)
+            recall += recall_score(pred, gold)
+    em = em * 100 / (len(preds) + 1e-5)
+    recall = recall * 100 / (len(preds) + 1e-5)
+    return em, recall
+
+def qa_to_prompt(query, context):
+    prompt = '{}\nQ: {}\nA: '.format(context, query)
+    return prompt
+
+def qa_to_prompt_baseline(query, context, schema):
+    def get_prompt(query, context, schema, answer=''):
+        if schema == 'base':
+            prompt = '{}\nQ:{}\nA:{}'.format(context, query, answer)
+        elif schema == 'opin':
+            context = context.replace('"', "")
+            prompt = 'Bob said "{}"\nQ: {} in Bob\'s opinion?\nA:{}'.format(context, query[:-1], answer)
+        elif schema == 'instr+opin':
+            context = context.replace('"', "")
+            prompt = 'Bob said "{}"\nQ: {} in Bob\'s opinion?\nA:{}'.format(context, query[:-1], answer)
+        elif schema == 'attr':
+            prompt = '{}\nQ:{} based on the given tex?\nA:{}'.format(context, query[:-1], answer)
+        elif schema == 'instr':
+            prompt = '{}\nQ:{}\nA:{}'.format(context, query, answer)
+        return prompt
+    prompt = ''
+    if schema in ('instr', 'instr+opin'):
+        prompt = 'Instruction: read the given information and answer the corresponding question.\n\n'
+    prompt = prompt + get_prompt(query, context, schema=schema)
+    return prompt
+
+    
+def eval(pred_answers, orig_answers, gold_answers):
+    em, ps = get_score(pred_answers, gold_answers)
+    _, po = get_score(pred_answers, orig_answers)
+    mr = po / (ps + po + 1e-10) * 100
+    print('ps {}, po {}, mr {}, em {}.'.format(ps, po, mr, em))
+
+def eval_case(pred_answers, orig_answers, gold_answers):
+    em, ps = get_score(pred_answers, gold_answers)
+    _, po = get_score(pred_answers, orig_answers)
+    return ps, po
+    
+def create_log_path(log_path):
+    if not os.path.exists(log_path):
+        with open(log_path, 'w') as f:
+            f.write('') 
+        logging.info(f"Log file {log_path} created.")
+    else:
+        logging.info(f"Log file {log_path} already exists.")
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", default="Models/Llama-3-8B-Instruct", type=str)
+    parser.add_argument("--orig_path", default="work7/faith/datasets/nq/orig_dev_filtered.json", type=str)
+    parser.add_argument("--counter_path", default="work7/faith/datasets/nq/conflict_dev_filtered.json", type=str)
+    parser.add_argument("--engine", default="text-davinci-003", type=str)
+    parser.add_argument("--schema", default="base", type=str, help="Choose from the following prompting templates: base, attr, instr, opin, instr+opin.")
+    parser.add_argument("--demo_mode", default="none", help="Choose from the following demonstrations: none, original, counter.")
+    parser.add_argument("--num_demos", default=16, type=int)
+    parser.add_argument("--log_path", default='work7/case_study/qwen_0.0.json', type=str)
+    parser.add_argument("--num-gpus", type=str, default="1")
+    parser.add_argument("--max_gpu_memory", type=int, default=27)
+    parser.add_argument("--device", type=str, choices=["cuda", "cpu"], default="cuda")
+    parser.add_argument('--mode', type=str, default='deck', 
+                    help='deck, base_rag, base_no_rag')
+    args = parser.parse_args()
+    with open(args.orig_path, 'r') as fh:
+        orig_examples = json.load(fh)
+    with open(args.counter_path, 'r') as fh:
+        counter_examples = json.load(fh)
+    print('Loaded {} instances.'.format(len(counter_examples)))
+
+    orig_examples = orig_examples[600:800]
+    counter_examples = counter_examples[600:800]
+
+    model_name = args.model_name
+    num_gpus = args.num_gpus
+    device = args.device
+    model = DECK(model_name, device, num_gpus, max_gpu_memory=27)
+    stop = []
+    model.set_stop_words(stop)
+
+    params_dict = {
+            "repetition_penalty": 1.0,
+            "temperature": 1.0,
+            "top_p": 1.0,
+            "top_k": 100,
+            "max_new_tokens": 64,
+            "logprobs": None,
+            "mode": args.mode,
+        }
+    
+    step = 0
+    gold_answers, pred_answers, orig_answers = [], [], []
+    record = []
+    for oe, ce in tqdm(zip(orig_examples, counter_examples), total=len(orig_examples)):
+        if step % 10 == 0:
+            eval(pred_answers, orig_answers, gold_answers)
+        step += 1
+        query, context, answer = ce['question'], ce['context'], ce['answer']
+        orig_answer = oe['answer']
+        if orig_answer is None:
+            continue
+        query = 'Q: {}\nA: '.format(query)
+        prompt = qa_to_prompt_baseline(query, context, schema=args.schema)
+        # pred = call_llama(model, tokenizer, prompt, stopping_criteria, stop)
+        pred = call_deck(model, query, prompt, stop, params_dict)
+        if pred is None:
+            continue
+        pred_answers.append(pred)
+        gold_answers.append(answer)
+        orig_answers.append(orig_answer)
+        # Logs
+        ce['prediction'] = pred
+        ce['orig_answer'] = orig_answer
+        ce['schema'] = args.schema
+        ce['demo_mode'] = args.demo_mode
+
+    if args.log_path:
+        with open(args.log_path, 'w') as fh:
+            json.dump(record, fh, indent=4,)
+    print("The parameter configuration is as follows:")
+    for arg, value in vars(args).items():
+        print(f"{arg}: {value}")
+    eval(pred_answers, orig_answers, gold_answers)
+
+    
+
+if __name__ == '__main__':
+    main()
